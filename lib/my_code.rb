c7b7691b7ccf0fce2e95b8f6e019f24c82308758
Anna Dybas <a.m.dybas@gmail.com>
2019-12-04 05:09:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,12 @@
 # My Code here....
 def map_to_negativize(arg)
   #arg.map { |number| -(number) }
+  negativized_array = []
+  i = 0
+  while i < arg.length do
+    negativized_array << arg[i] * -1
+  end
+  negativized_array
 end
 
 def map_to_no_change(arg)
